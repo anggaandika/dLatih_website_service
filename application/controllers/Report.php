3e5396df72e3421e48ace82026adef5994ff2b4b
RizkiFadhoni <48019329+RizkiFadhoni@users.noreply.github.com>
2019-05-02 16:26:57 +0700

    Tampilan Report

---
+++ application/controllers/Report.php
@@ -0,0 +1,35 @@
+<?php
+class Report extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('m_Auth', 'a');
+        $this->load->model('m_User', 'u');
+
+        is_logged_in();
+    }
+    public function index()
+    {
+        //session data email
+        $log = $this->session->userdata('email');
+        //data user sesuai dari session
+        $data['user'] = $this->a->getAllUser($log);
+        //tampilan dashboard
+        $this->load->view('--temp/-header', $data);
+        $this->load->view('report/list_report', $data);
+        $this->load->view('--temp/-footer');
+    }
+    public function reply()
+    {
+        $log = $this->session->userdata('email');
+        //data jumlah dalam angka
+
+        //data user sesuai dari session
+        $data['user'] = $this->a->getAllUser($log);
+        $this->load->view('--temp/-header', $data);
+        $this->load->view('report/reply_report');
+        $this->load->view('--temp/-footer');
+    }
+}
